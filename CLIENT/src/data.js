9081fc12ad0254c527f76f78c59bddf293a833a0
Luis Hernandez <abdalaluis2@gmail.com>
2023-01-31 18:18:18 +0000

    Added photos to subCats carrousel

--- CLIENT/src/data.js
+++ CLIENT/src/data.js
@@ -109,44 +109,45 @@ export const subCats = [
   {
     id: 10,
     title: "Poleras",
-    img: "https://images.unsplash.com/photo-1484517586036-ed3db9e3749e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    img: "https://images.unsplash.com/photo-1532296075534-cc428d395281?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
     value: "poleras",
   },
   {
     id: 12,
     title: "Tazas",
 
-    img: "https://images.unsplash.com/photo-1614940403511-e4e3d39df8c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80",
+    img: "https://images.unsplash.com/photo-1474667689933-0ff72b3d16e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     value: "tazas",
   },
   {
     id: 13,
     title: "Polerones",
-    img: "https://images.unsplash.com/photo-1543087903-1ac2ec7aa8c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1498&q=80",
+    img: "https://images.unsplash.com/photo-1485230895905-ec40ba36b9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     value: "polerones",
   },
   {
     id: 14,
     title: "Gorras",
-    img: "https://images.unsplash.com/photo-1614940403511-e4e3d39df8c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80",
+    img: "https://images.unsplash.com/photo-1519079754742-f83afaef6d35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     value: "gorras",
   },
   {
     id: 15,
     title: "Shorts",
-    img: "https://images.unsplash.com/photo-1614940403511-e4e3d39df8c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80",
+    img: "https://images.unsplash.com/photo-1534957695859-1a634d9ca58a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80",
     value: "shorts",
   },
   {
     id: 16,
     title: "Cojines",
-    img: "https://images.unsplash.com/photo-1614940403511-e4e3d39df8c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80",
+    img: "https://images.unsplash.com/photo-1586892378222-2f53ce5d609a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
     value: "cojines",
   },
   {
     id: 17,
     title: "Mochilas",
-    img: "https://images.unsplash.com/photo-1614940403511-e4e3d39df8c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80",
+    img: "https://images.unsplash.com/photo-1495968283540-e1df41995ba6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+
     value: "mochilas",
   },
 ];
